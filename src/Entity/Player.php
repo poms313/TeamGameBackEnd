ae3915b66c66a6b9dc9a8c02b1df45980b67971e
Pommine fillatre <44481977+poms313@users.noreply.github.com>
2022-02-23 15:48:40 +0100

    create database and players

---
+++ src/Entity/Player.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlayerRepository::class)]
+class Player
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $strength;
+
+    #[ORM\Column(type: 'integer')]
+    private $memory;
+
+    #[ORM\Column(type: 'integer')]
+    private $intelligence;
+
+    #[ORM\Column(type: 'integer')]
+    private $logic;
+
+    #[ORM\Column(type: 'integer')]
+    private $resistance;
+
+    #[ORM\Column(type: 'integer')]
+    private $fighting;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(int $strength): self
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    public function getMemory(): ?int
+    {
+        return $this->memory;
+    }
+
+    public function setMemory(int $memory): self
+    {
+        $this->memory = $memory;
+
+        return $this;
+    }
+
+    public function getIntelligence(): ?int
+    {
+        return $this->intelligence;
+    }
+
+    public function setIntelligence(int $intelligence): self
+    {
+        $this->intelligence = $intelligence;
+
+        return $this;
+    }
+
+    public function getLogic(): ?int
+    {
+        return $this->logic;
+    }
+
+    public function setLogic(int $logic): self
+    {
+        $this->logic = $logic;
+
+        return $this;
+    }
+
+    public function getResistance(): ?int
+    {
+        return $this->resistance;
+    }
+
+    public function setResistance(int $resistance): self
+    {
+        $this->resistance = $resistance;
+
+        return $this;
+    }
+
+    public function getFighting(): ?int
+    {
+        return $this->fighting;
+    }
+
+    public function setFighting(int $fighting): self
+    {
+        $this->fighting = $fighting;
+
+        return $this;
+    }
+}
