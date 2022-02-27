915678f7f923501ceff65f6063889d9c0ea8ad84
Pommine fillatre <44481977+poms313@users.noreply.github.com>
2022-02-27 18:40:31 +0100

    added a function that generates 10 players

--- src/Controller/ListAllPlayersController.php
+++ src/Controller/ListAllPlayersController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Player;
 use App\Repository\PlayerRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ListAllPlayersController extends AbstractController
 {
-  public function __construct(PlayerRepository $playerRepository)
+
+  public function __construct(PlayerRepository $playerRepository, ManagerRegistry $doctrine)
   {
     $this->playerRepository = $playerRepository;
+    $this->doctrine = $doctrine;
   }
 
   /**
@@ -23,11 +27,33 @@ class ListAllPlayersController extends AbstractController
     $dbPlayersList = $this->playerRepository->findAll();
     $playerListForConverting = array();
 
+    if (empty($dbPlayersList)) {
+      $dbPlayersList = $this->generatePlayers();
+    }
+
     foreach ($dbPlayersList as $player) {
       $playerListForConverting[] = $player->buildArray();
     }
     return new JsonResponse(
       $playerListForConverting
     );
+  }
+
+  public function generatePlayers()
+  {
+    $playersName = array('Paul', 'Ingrid', 'Eric', 'Marion', 'Sophie', 'Christelle', 'Nadia', 'Benoit', 'Claude', 'Claudia');
+
+    foreach ($playersName as $item) {
+      $newPlayer = new Player();
+      $newPlayer->setName($item);
+      $newPlayer->setStrength(rand(1, 100));
+      $newPlayer->setMemory(rand(1, 100));
+      $newPlayer->setIntelligence(rand(1, 100));
+      $newPlayer->setLogic(rand(1, 100));
+      $newPlayer->setResistance(rand(1, 100));
+      $newPlayer->setFighting(rand(1, 100));
+      $entityManager = $this->doctrine->getManager();
+      $entityManager->flush();
+    }
   }
 }
